820393b5bfc68894ec683ce99ca345e07683198d
Tillmann Weimer <tillmann1970@gmail.com>
2020-10-19 15:57:24 +0200

    created functions for creating and deleting items

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const addToForm = document.querySelector("#addTodoForm")
 const addItem = document.querySelector("#addTodoInputField")
+const showItem = document.querySelector("#todoList")
 
 //add eventListener for submitting form
 addToForm.focus()
 addToForm.addEventListener("submit", e => {
   e.preventDefault()
-  console.log(addItem.value)
+  //console.log(addItem.value)
+  createItem(addItem.value)
 })
+
+function createItem(x) {
+  let ourHtml = `<li>${x}<button onClick="deleteItem(this)">Delete</button></li>`
+  showItem.insertAdjacentHTML("beforeend", ourHtml)
+  addItem.value = ""
+}
+
+function deleteItem(elementToDelete) {
+  elementToDelete.parentElement.remove()
+}
